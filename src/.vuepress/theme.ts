bd305cb2ee4daaed4b3741921ca218f2cf7307ee
Mr.Hope <mister-hope@outlook.com>
2023-05-16 14:38:06 +0800

    chore: update deps

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -33,13 +33,6 @@ export default hopeTheme(
       name: "VuePress Theme Hope",
     },
 
-    themeColor: {
-      blue: "#2196f3",
-      red: "#f26d6d",
-      green: "#3eaf7c",
-      orange: "#fb9b5f",
-    },
-
     fullscreen: true,
 
     locales: {
@@ -137,5 +130,5 @@ export default hopeTheme(
       },
     },
   },
-  false
+  { custom: true }
 );
